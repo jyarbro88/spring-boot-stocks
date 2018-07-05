90e52e7631858d445139bb75d7047a5c370e8bac
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-05 09:36:05 -0500

    setup abstract repo

---
+++ src/main/java/com/springboot/stocks/datalayer/AbstractRepository.java
@@ -0,0 +1,56 @@
+package com.springboot.stocks.datalayer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityResult;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class AbstractRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Transactional
+    public void persist(Object entity) {
+        entityManager.persist(entity);
+    }
+
+    @Transactional
+    public void remove(Object entity) {
+        entityManager.remove(entity);
+    }
+
+    @Transactional
+    public <T> T merge(T entity) {
+        return entityManager.merge(entity);
+    }
+
+    @Transactional
+    public <T> List<T> list(Class<T> tClass) {
+        String sql = "SELECT t FROM " + tClass.getName() + " t";
+        List<T> ts = getEntityManager().createQuery(sql, tClass).getResultList();
+
+        return ts;
+    }
+
+    public <T> T find(Class<T> tClass, Long id) {
+        String sql = "SELECT t FROM " + tClass.getName() + " t WHERE t.id = :id";
+        TypedQuery<T> query = getEntityManager().createQuery(sql, tClass);
+        query.setParameter("id", id);
+
+        List<T> ts = query.getResultList();
+        return (ts.isEmpty() ? null : ts.get(0));
+    }
+}
--- src/main/java/com/springboot/stocks/datalayer/StockDAOMapper.java
+++ src/main/java/com/springboot/stocks/datalayer/StockDAOMapper.java
@@ -1,4 +1,34 @@
 package com.springboot.stocks.datalayer;
 
+import com.springboot.stocks.weblayer.beans.StockBean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class StockDAOMapper {
+
+    public StockDAO toDAO(StockBean stockBean) {
+        StockDAO stockDAO = new StockDAO();
+        updateDAO(stockBean, stockDAO);
+
+        return stockDAO;
+    }
+
+    private void updateDAO(StockBean stockBean, StockDAO stockDAO) {
+
+        if (stockBean.getSymbol() != null) {
+            stockDAO.setSymbol(stockBean.getSymbol());
+        }
+
+        if (stockBean.getPrice() != null) {
+            stockDAO.setPrice(stockBean.getPrice());
+        }
+
+        if (stockBean.getVolume() != null) {
+            stockDAO.setVolume(stockBean.getVolume());
+        }
+
+        if (stockBean.getDate() != null) {
+            stockDAO.setDate(stockBean.getDate());
+        }
+    }
 }
