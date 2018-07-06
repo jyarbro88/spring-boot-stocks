b54bf04f3d3992f1227d3c188c2a33c3610e3a73
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-06 11:35:00 -0500

    cleaned up reader

--- src/main/java/com/springboot/stocks/JsonReader.java
+++ src/main/java/com/springboot/stocks/JsonReader.java
@@ -1,7 +1,7 @@
 package com.springboot.stocks;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.springboot.stocks.datalayer.StockDAO;
+import com.springboot.stocks.datalayer.stock_quotes;
 import com.springboot.stocks.datalayer.StockRepository;
 
 import java.io.File;
@@ -11,7 +11,7 @@ class JsonReader {
     void readJsonFile(StockRepository stockRepository) throws Exception {
 
         ObjectMapper objectMapper = new ObjectMapper();
-        StockDAO[] stockList = objectMapper.readValue(new File("/Users/joeyarbrough/Projects/Spring-Boot-Stocks/src/main/resources/stocks.json"), StockDAO[].class);
+        stock_quotes[] stockList = objectMapper.readValue(new File("/Users/joeyarbrough/Projects/Spring-Boot-Stocks/src/main/resources/stocks.json"), stock_quotes[].class);
         int counter = 0;
         while (counter < (stockList.length - 1)) {
             counter++;
